0fd4e4e2b81e19b921f038f76748f4afa5a22b24
Seongjun Kwon <tttzkq9336@gmail.com>
2024-02-26 03:26:54 +0900

    first commit

---
+++ src/main/java/fisa/edu/Kwonchelin/KwonchelinApplication.java
@@ -0,0 +1,13 @@
+package fisa.edu.Kwonchelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwonchelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwonchelinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fisa/edu/Kwonchelin/KwonchelinApplicationTests.java
@@ -0,0 +1,13 @@
+package fisa.edu.Kwonchelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwonchelinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
